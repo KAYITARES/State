088d98bef6514698083ec673626d701eafb4c175
KAYITARES <cyntkayitare@gmail.com>
2023-12-08 11:15:25 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
+const message = ["Learn HTML & CSS", "Learn React JS", "Learn Node JS"];
 function App() {
+  const [step, setStep] = useState(1);
+
+  function handleNext() {
+    if (step <= 2) {
+      setStep(step + 1);
+    }
+  }
+  function handlePreview() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={step === 1 ? "active" : ""}>1</div>
+        <div className={step === 2 ? "active" : ""}>2</div>
+        <div className={step === 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {message[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button onClick={handlePreview}>Preview</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
     </div>
   );
 }
