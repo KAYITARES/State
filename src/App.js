37c3e478c30ca40eefb7514af29a356b83c2b9a1
KAYITARES <cyntkayitare@gmail.com>
2023-12-14 16:31:08 +0200

    add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 const message = ["Learn HTML & CSS", "Learn React JS", "Learn Node JS"];
 function App() {
   const [step, setStep] = useState(1);
+  const [open, setOPen] = useState(true);
+  const [count, setCount] = useState(0);
 
   function handleNext() {
     if (step <= 2) {
@@ -15,23 +17,45 @@ function App() {
       setStep(step - 1);
     }
   }
+  function hanleCount() {
+    if (step === count) {
+      setCount(count + step);
+    } else {
+      setCount(count + 1);
+    }
+  }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step === 1 ? "active" : ""}>1</div>
-        <div className={step === 2 ? "active" : ""}>2</div>
-        <div className={step === 3 ? "active" : ""}>3</div>
+    <>
+      <div className="btn-cont">
+        <button
+          className="btn"
+          onClick={() => {
+            setOPen(!open);
+          }}
+        >
+          &times;
+        </button>
       </div>
-      <p className="message">
-        Step {step}: {message[step - 1]}
-      </p>
-
-      <div className="buttons">
-        <button onClick={handlePreview}>Preview</button>
-        <button onClick={handleNext}>Next</button>
-      </div>
-    </div>
+      {open && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step === 1 ? "active" : ""}>1</div>
+            <div className={step === 2 ? "active" : ""}>2</div>
+            <div className={step === 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {message[step - 1]}
+          </p>
+          <p>{count}</p>
+          <div className="buttons">
+            <button onClick={handlePreview}>Preview</button>
+            <button onClick={handleNext}>Next</button>
+            <button onClick={hanleCount}>Add</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
